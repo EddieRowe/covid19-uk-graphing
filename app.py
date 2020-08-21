3dfdd1af590a88ac7177104123f480ac95eddc92
EddieRowe <edrowe@pm.me>
2020-08-21 10:08:46 +0100

    Formatting, refactoring, all remaining CSS in app.py moved to styles.css

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.title = "COVID-19 UK Data Graphing"
    
 
 ### PHE API stuff
-# specify data to grab
+# Specify data to grab
 datastructure = {
     "Date": "date",
     "Area": "areaName",
@@ -20,7 +20,7 @@ datastructure = {
     "Cases": "newCasesByPublishDate"
 }
 
-# specify region type (nation/region/ltla)
+# Specify region type (nation/region/ltla)
 all_uk = [
     'areaType=nation'
 ]
@@ -29,40 +29,40 @@ all_uk = [
 phe_api = Cov19API(filters=all_uk, structure=datastructure)
 data = phe_api.get_json()
 
-# latest website update
+# Latest website update
 release_timestamp = Cov19API.get_release_timestamp()
 
-# date formatting
-release_timestamp_formatted = release_timestamp.replace('Z', '')
-date = datetime.fromisoformat(release_timestamp_formatted)
-date_time = date.strftime("%d/%m/%Y, %H:%M")
-date_ = date.strftime("%d-%m-%Y")
-datestring = "Latest PHE data from ", date_time, " GMT."
-### end of PHE API stuff
+# Date formatting
+release_timestamp = release_timestamp.replace('Z', '')
+date = datetime.fromisoformat(release_timestamp)
+date_and_time = date.strftime("%d/%m/%Y, %H:%M")
+date = date.strftime("%d-%m-%Y")
+datestring = "Latest PHE data from ", date_and_time, " GMT."
+### End of PHE API stuff
 
-# specify dataframe
+# Specify dataframe
 df = data['data'];
-# convert to pandas dataframe
+# Convert to pandas dataframe
 df = pd.DataFrame(df)
 
 
-# line graph of deaths by date
+# Line graph of deaths by date
 fig = px.line(df, x="Date", y="Deaths", color="Area", 
               title="Deaths by Nation", 
               width=600, height=350,
               template="plotly_dark"
 )
 
-# set style elements
+# Set style elements
 fig.update_layout(hovermode='x unified', 
                   plot_bgcolor='#232627', 
                   paper_bgcolor='#232627'
 )
 
-# set hover style
+# Set hover style
 fig.update_traces(hovertemplate = '<br>Deaths: %{y}')
 
-# line graph of cases by date
+# Line graph of cases by date
 fig2 = px.line(df, x="Date", y="Cases", color="Area", 
                title="Cases by Nation", 
                width=600, height=350,
@@ -80,7 +80,7 @@ fig2.update_traces(hovertemplate = '<br>Cases: %{y}')
 df['Deaths'] = df['Deaths'].rolling(window=7).mean()
 df['Cases'] = df['Cases'].rolling(window=7).mean()
 
-# line graph of deaths by date (7 day rolling avg)
+# Line graph of deaths by date (7 day rolling avg)
 fig3 = px.line(df, x="Date", y="Deaths", color="Area", 
                title="Deaths by Nation (7-day rolling average)", 
                width=600, height=350,
@@ -94,7 +94,7 @@ fig3.update_layout(hovermode='x unified',
 
 fig3.update_traces(hovertemplate = '<br>Deaths: %{y}')
 
-# line graph of cases by date (7 day rolling avg)
+# Line graph of cases by date (7 day rolling avg)
 fig4 = px.line(df, x="Date", y="Cases", color="Area", 
                title="Cases by Nation (7-day rolling average)", 
                width=600, height=350,
@@ -110,27 +110,27 @@ fig4.update_traces(hovertemplate = '<br>Cases: %{y}')
 
 
 # Webpage layout, (css is loaded from assets/styles.css)
-app.layout = html.Div(children=
+app.layout = html.Div(
     [
-    
         # Title and general information
-        html.H1(children='COVID-19 UK Data Graphing', 
-                style={'textAlign': 'center'}
-        ),
+        html.Div(
+            className="centeredtext",
+            children=[
+                html.H1('COVID-19 UK Data Graphing'),
 
-        html.P(children='Graphs generated using the latest data from Public Health England.', 
-            style={'textAlign': 'center'}
+                html.P('Graphs generated using the latest data from Public Health England.'),
+                html.P(datestring),
+                
+                dcc.Link('View source on GitHub.', 
+                        href='https://github.com/EddieRowe/covid19-uk-graphing', 
+                        target='_blank')
+            ]
         ),
-
-        html.P(children=datestring, style={'textAlign': 'center'}),
         
-        html.Div(children=dcc.Link('View source on GitHub.', href='https://github.com/EddieRowe/covid19-uk-graphing', target='_blank'), 
-                style={'textAlign': 'center'}
-        ),
-            
         # Insert graphs
         html.Div(
-            [
+            className="graphs",
+            children=[
                 dcc.Graph(
                     id='chart_cases_line',
                     figure=fig2,
@@ -148,12 +148,12 @@ app.layout = html.Div(children=
                         'modeBarButtonsToRemove':['toggleSpikelines', 'zoomIn2d', 'zoomOut2d', 'autoScale2d']
                     }
                 )
-            ], 
-            style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
+            ]
         ),
             
         html.Div(
-            [
+            className="graphs",
+            children=[
                 dcc.Graph(
                     id='chart_deaths_line',
                     figure=fig,
@@ -171,18 +171,24 @@ app.layout = html.Div(children=
                     'modeBarButtonsToRemove':['toggleSpikelines', 'zoomIn2d', 'zoomOut2d', 'autoScale2d']
                     }
                 )
-            ], 
-            style = {'width': '100%', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}
+            ]
         ),
             
-            
-        html.Div(children=dcc.Link('code.eddierowe.com', href='https://code.eddierowe.com', target='_blank'), 
-                style={'textAlign': 'center'}
-        ),
-        
-        html.Br()
-    ], 
-    style={'margin': 'auto', 'width': '50%'}
+        # Footer
+        html.Div(
+            className="centeredtext",
+            children=[
+                html.P(
+                    [
+                        'Created by Eddie Rowe -  ',
+                        html.A('code.eddierowe.com', 
+                                href='https://code.eddierowe.com/', 
+                                target='_blank')
+                    ]
+                )
+            ]
+        )
+    ]
 )
 
 if __name__ == '__main__':
